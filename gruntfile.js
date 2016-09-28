292dea191893aee8b19fcc4200b412140b1fdb89
Per-Ragnar Jonsson <perragnar.jonsson@gmail.com>
2016-09-28 21:44:07 +0200

    New icons and page-fixes

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,23 @@
 module.exports = function(grunt) {
 
 	var fs = require("fs")
-	var files = '';
 	var filename = '';
-	var template = '';
+	var icons = '';
 	var counter = 0;
 	var list = function (path) {
 	fs.readdirSync(path).forEach(function (file) {
 			filename = file.replace('.svg', '');
-			template = '<div class="icon"><img src="icons/' + file + '" width="24" height="24" /><span class="search">' + filename + '</span></div>'
-			files = files + template
+			icons = icons + '<div class="icon' + (filename === 'loader-ring' ? ' rotate' : '') + '"><img src="icons/' + file + '" width="24" height="24" /><span class="search">' + filename + '</span></div>'
 			counter++;
 		});
 	}
 	list('icons');
-	files = files + '<div class="num">' + counter + ' icons and counting</div>';
+	counter = '<div class="num">' + counter + ' icons and counting</div>';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		replace: {
-			remove: {
+			clean_icons: {
 				src: ['icons/*.svg'],
 				overwrite: true,
 				replacements: [{
@@ -33,12 +31,15 @@ module.exports = function(grunt) {
 					to: ' fill="#454545"'
 				}]
 			},
-			build: {
+			build_page: {
 				src: ['index.html'],
 				overwrite: true,
 				replacements: [{
-					from: /<div data-name.*/g,
-					to: files
+					from: /<div class="icon">.*/g,
+					to: icons
+				}, {
+					from: /<div class="num">.*>/g,
+					to: counter
 				}]
 			}
 		},
